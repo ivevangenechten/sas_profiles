c9837dd096ebf4ea6a7f608c3b2f515be9264079
Tom Sneyers <tom.sneyers@vito.be>
2014-08-05 16:02:30 +0200

    Added vmware tools archive url.

--- manifests/vmwaretools.pp
+++ manifests/vmwaretools.pp
@@ -1,11 +1,13 @@
 class profiles::vmwaretools {
 
   ## Hiera lookups
+  $archive_url = hiera('profiles::vmwaretools::archive_url')
   $version = hiera('profiles::vmwaretools::version')
   $tmp = hiera('profiles::vmwaretools::working_dir')
 
 
   class { '::vmwaretools':
+    archive_url => $archive_url,
     version => $version,
     working_dir => $tmp,
   }
