dad17a2f270dee4875fe74e0665381defe74fe15
Tom Sneyers <tom.sneyers@vito.be>
2014-08-18 11:18:38 +0200

    jenkins config

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,9 +1,31 @@
 class profiles::jenkins {
 
-class { '::jenkins':
-  configure_firewall => false,
-  install_java => false,
-}
+  ## Hiera lookups
+  $url = hiera('profiles::jenkins::url')
 
+  class { '::jenkins':
+    configure_firewall => false,
+    install_java => false,
+  }
+
+  class { '::apache':
+  }
+
+  a2mod { "Enable proxy mod":
+    name => "proxy",
+    ensure => "present",
+  }
+
+  a2mod { "Enable proxy_http mod":
+    name => "proxy_http",
+    ensure => "present",
+  }
+
+  apache::vhost::proxy { "jenkins":
+    servername => "${url}",
+    port => 80,
+    dest => "http://${$url}:8080",
+    vhost_name => "jenkins",
+  }
 
 }
