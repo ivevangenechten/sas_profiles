fafa8a4c98d6040e5e5ee62dd8b12325e3f341d2
Tom Sneyers <tom.sneyers@vito.be>
2014-08-29 09:47:59 +0200

    jenkins config

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -9,6 +9,9 @@ class profiles::jenkins {
   }
 
   class { '::apacheproxy':
+    server_admin => 'tom.sneyers@vito.be',
+    server_name => 'jenkins.sas.vito.local',
+    destination_url => 'http://jenkins.sas.vito.local:8080/',
   }
 
 
