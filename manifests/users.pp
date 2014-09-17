1104bb5453569fb5c289903065f929317e14eb44
Tom Sneyers <tom.sneyers@vito.be>
2014-09-17 09:14:35 +0200

    ssh authorized keys added

--- manifests/users.pp
+++ manifests/users.pp
@@ -3,8 +3,10 @@ class profiles::users {
   ## Hiera lookups
   $users = hiera_hash('profiles::users')
   $groups = hiera_hash('profiles::groups')
+  $ssh_authorized_keys = hiera_hash('profiles::ssh_authorized_keys')
 
   create_resources('user', $users)
   create_resources('group', $groups)
+  create_resources('ssh_authorized_keys', $ssh_authorized_keys)
 
 }
