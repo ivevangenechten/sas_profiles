b451ed3cfb544ba8c26b1618ea2179e6d84092b0
Tom Sneyers <tom.sneyers@vito.be>
2014-09-24 10:32:30 +0200

    added ensure present

--- manifests/sshkeys.pp
+++ manifests/sshkeys.pp
@@ -7,11 +7,13 @@ class profiles::sshkeys {
   host_aliases => [ "$fqdn", "$hostname", "$ipaddress" ],
   type => dsa,
   key  => $sshdsakey,
+  ensure => present,
 }
 @@sshkey { "${fqdn}_rsa":
   host_aliases => [ "$fqdn", "$hostname", "$ipaddress" ],
   type => rsa,
   key  => $sshrsakey,
+  ensure => present,
 }
 
 # Collect:
