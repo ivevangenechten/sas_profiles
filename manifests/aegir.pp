9b433a777a196ae841719bae6c687a851189ce27
Tom Sneyers <tom.sneyers@vito.be>
2014-10-28 10:11:27 +0100

    updated aegir profile

--- manifests/aegir.pp
+++ manifests/aegir.pp
@@ -1,7 +1,13 @@
-class profiles::aegir
-{
+class profiles::aegir (
+  $install_aegir   = true,
+  $install_mysql   = true,
+  $install_apache2 = true
+) {
 
- class {'::aegir':
- }
+  class { '::aegir':
+    install_aegir    => profiles::aegir::install_aegir,
+    install_mysql    => profiles::aegir::install_mysql,
+    install_apache2  => profiles::aegir::install_apache2
+  }
 
 }
