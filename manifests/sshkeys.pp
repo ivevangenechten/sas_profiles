fa860049e16df9db3cbfa59f15485c05bc110ae6
Tom Sneyers <tom.sneyers@vito.be>
2014-10-30 11:31:45 +0100

    empty

--- manifests/sshkeys.pp
+++ manifests/sshkeys.pp
@@ -1,22 +1,5 @@
 class profiles::sshkeys {
 
-# Every node with the ssh class will export its own SSH host key and then collect the SSH host key of every node (including its own). This will cause every node in the site to trust SSH connections from every other node.
 
-# Declare:
-  @@sshkey { "${fqdn}_dsa":
-    host_aliases => [ "$fqdn", "$hostname", "$ipaddress" ],
-    type => dsa,
-    key  => $sshdsakey,
-    ensure => present,
-  }
-  @@sshkey { "${fqdn}_rsa":
-    host_aliases => [ "$fqdn", "$hostname", "$ipaddress" ],
-    type => rsa,
-    key  => $sshrsakey,
-    ensure => present,
-  }
-
-# Collect:
-  Sshkey <<| |>>
 
 }
