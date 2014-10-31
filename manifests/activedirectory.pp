419971298b7e82c67ea4a2c0ee7cd74735e0ccd7
Tom Sneyers <tom.sneyers@vito.be>
2014-10-31 08:55:12 +0100

    corrected profiles module dir

--- manifests/activedirectory.pp
+++ manifests/activedirectory.pp
@@ -26,7 +26,7 @@ class profiles::activedirectory {
 
   file { '/etc/samba/smb.conf':
     path    => '/etc/samba/smb.conf',
-    source  => "puppet:///modules/sas_profiles/smb.conf",
+    source  => "puppet:///modules/profiles/smb.conf",
     require => Package['samba'],
     owner   => root, group => 0, mode => 0644,
     notify  => Service['smbd'],
@@ -34,7 +34,7 @@ class profiles::activedirectory {
 
   file { '/etc/nsswitch.conf':
     path    => '/etc/nsswitch.conf',
-    source  => "puppet:///modules/sas_profiles/nsswitch.conf",
+    source  => "puppet:///modules/profiles/nsswitch.conf",
     require => Package['winbind'],
     owner   => root, group => 0, mode => 0644,
     notify  => Service['winbind'],
